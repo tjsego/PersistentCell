6b5185174fce401d1a6472b4f8c2e455c5c77093
Randy Heiland <randy.heiland@gmail.com>
2024-06-17 08:59:34 -0400

    update

--- implementations/physicell/plot_pos.py
+++ implementations/physicell/plot_pos.py
@@ -3,16 +3,44 @@ import matplotlib.pyplot as plt
 
 info_dict = {}
 xy = []
-full_fname="path_001.mat"
-for idx in range(0,6):
-    full_fname= f'path_{idx:03d}.mat'
-    print(full_fname)
-    scipy.io.loadmat(full_fname, info_dict)
+full_fname="paths_all.mat"
+scipy.io.loadmat(full_fname, info_dict)
 # print(info_dict['cell_pos'])
     # xy = info_dict['cell_pos']
-    xy.append(info_dict['cell_pos'])
-# print("xy.shape")
+xy = info_dict['cell_pos']
+print("xy.shape = ",xy.shape)  # (315, 2) for 3 brief plots; each not necessarily same length
     # plt.plot(xy[:,0],xy[:,1],'gray',linewidth=0.5)
-    plt.plot(xy[idx][:,0], xy[idx][:,1],'gray',linewidth=0.5)
-plt.ylim((-100,100))
+ix0 = [0]
+print(ix0)
+for idx in range(1, xy.shape[0]):
+    # xn = xy[idx,0] 
+    # xn_m1 = xy[idx-1, 0]
+    # if xn < xn_m1:
+    if xy[idx,0] == -99.0:
+        # print(f'{xn} < {xn_m1}')
+        print(f'--- found -99 terminator token')
+        ix0.append(idx)
+
+print("ix0= ",ix0)
+istart = 0
+iend = ix0[1]
+print(f"plot from {istart} to {iend} --> del={iend-istart}")
+plt.plot(xy[istart:iend,0], xy[istart:iend,1],'gray',linewidth=0.5)
+
+for idx in range(1,len(ix0)-1):
+    istart = ix0[idx] + 1
+    iend = ix0[idx+1]
+    if istart == iend:
+        break
+    print(f"plot from {istart} to {iend} --> del={iend-istart}")
+    plt.plot(xy[istart:iend,0], xy[istart:iend,1],'gray',linewidth=0.5)
+# plt.ylim((-100,100))
+# istart = iend
+# iend = xy.shape[0]
+# print(f"plot from {istart} to {iend} --> del={iend-istart}")
+# plt.plot(xy[istart:iend,0], xy[istart:iend,1],'gray',linewidth=0.5)
+plt.xlim([0, 1000])
+# plt.ylim([-100, 100])
+plt.ylim([-50, 50])
+plt.title("PhysiCell migration bias = 0.8")
 plt.show()
