c5c19fe330b258273d69a44c9fec95880874b15d
tjsego <tjsego@gmail.com>
2025-05-06 17:06:48 -0400

    Workflow fix

--- workflow/run_compare.py
+++ workflow/run_compare.py
@@ -10,9 +10,14 @@ from matplotlib import pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import sys
 from typing import Any, Dict, List, Type
 
 from workflow import basic
+
+sys.path.append(basic.dir_compare)
+
+from ssr.basic import load_results
 
 logger = logging.getLogger(__name__)
 
@@ -25,14 +30,15 @@ def _log_error(msg: str, err_type: Type[BaseException]):
     raise err_type(msg)
 
 
-def _compare_reports(_modeler_rep: libssr.EFECTReport,
+def _compare_results(_modeler_res: Dict[str, np.ndarray],
                      _curator_rep: libssr.EFECTReport,
                      _curator_smp: List[float]):
     err_granular = {name: [] for name in _curator_rep.variable_names}
     for i, name in enumerate(_curator_rep.variable_names):
         for j in range(_curator_rep.simulation_times.shape[0]):
-            err_granular[name].append(libssr.ecf_compare(_modeler_rep.ecf_evals[j, i, :, :],
-                                                         _curator_rep.ecf_evals[j, i, :, :]))
+            ecf_res = libssr.ecf(libssr.round_arr_to_sigfigs(_modeler_res[name][:, j], _curator_rep.sig_figs),
+                                 libssr.get_eval_info_times(_curator_rep.ecf_nval, _curator_rep.ecf_tval[j, i]))
+            err_granular[name].append(libssr.ecf_compare(ecf_res, _curator_rep.ecf_evals[j, i, :, :]))
     err_names = {n: max(v) for n, v in err_granular.items()}
     err_res = max(err_names.values())
     return {
@@ -44,9 +50,12 @@ def _compare_reports(_modeler_rep: libssr.EFECTReport,
 
 
 def _post_summary_name(_data: Dict[str, float],
+                       _pval: float,
                        _output_dir: str,
                        _output_fexts: List[str],
-                       _dpi: int):
+                       _dpi: int,
+                       _modeler_impl: str,
+                       _curator_impl: str):
 
     names = list(_data.keys())
     values = [_data[n] for n in names]
@@ -61,9 +70,10 @@ def _post_summary_name(_data: Dict[str, float],
     )
     ax.legend().set_visible(False)
     ax.set_ylabel('EFECT Error')
+    ax.set_title(f'{_modeler_impl}/{_curator_impl} p-val={_pval}')
     ax.set_ylim(0, 2)
     ax.axhline(y=efect_error, color='black', linestyle='--')
-    ax.annotate(f'EFECT Error={efect_error}', xy=(0, efect_error), xytext=(0, efect_error + 0.25),
+    ax.annotate(f'EFECT Error={efect_error:.2f}', xy=(0, efect_error), xytext=(0, efect_error + 0.25),
                 arrowprops=dict(facecolor='black', shrink=0.05))
 
     output_name = 'summary'
@@ -96,6 +106,42 @@ def _post_summary_granular(_data: Dict[str, List[float]],
         ax.set_ylabel('EFECT Error')
 
     output_name = 'granular'
+    for fext in _output_fexts:
+        fig.savefig(os.path.join(_output_dir, output_name + '.' + fext),
+                    dpi=_dpi)
+    plt.close(fig)
+
+
+def _post_all(_data: Dict[str, Dict[str, float]],
+              _output_dir: str,
+              _output_fexts: List[str],
+              _dpi=basic.post_dpi):
+
+    names = sorted(_data.keys())
+    num_names = len(names)
+
+    heatmap_data = np.zeros((num_names, num_names))
+
+    for ni in names:
+        for nj in names:
+            heatmap_data[names.index(ni), names.index(nj)] = _data[ni][nj]
+
+    fig, ax = plt.subplots(1, 1,
+                           layout='compressed',
+                           figsize=(2.0 * num_names, 2.0 * num_names))
+    cax = ax.matshow(heatmap_data, cmap='viridis')
+    fig.colorbar(cax)
+
+    ax.set_xticks(list(range(num_names)))
+    ax.set_yticks(list(range(num_names)))
+    ax.set_xticklabels(names)
+    ax.set_yticklabels(names)
+
+    for i in range(num_names):
+        for j in range(num_names):
+            ax.text(j, i, f'{heatmap_data[i, j]:.2f}', ha='center', va='center', color='white')
+
+    output_name = 'all'
     for fext in _output_fexts:
         fig.savefig(os.path.join(_output_dir, output_name + '.' + fext),
                     dpi=_dpi)
@@ -148,13 +194,36 @@ def _post(_experiment_dir: str,
 
         output_data_job = output_data[job]
         _post_summary_name(output_data_job[basic.comparison_key_named_efect_error],
+                           output_data_job[basic.comparison_key_rej_pval],
                            output_dir_job,
                            _output_fexts,
-                           fig_dpi)
+                           fig_dpi,
+                           output_data_job[basic.comparison_key_modeler],
+                           output_data_job[basic.comparison_key_curator])
         _post_summary_granular(output_data_job[basic.comparison_key_granular_efect_error],
                                output_dir_job,
                                _output_fexts,
                                fig_dpi)
+
+    if output_data:
+
+        post_all_data = {}
+        for d in output_data:
+            impl_modeler = d[basic.comparison_key_modeler]
+            impl_curator = d[basic.comparison_key_curator]
+            pval = d[basic.comparison_key_rej_pval]
+            try:
+                post_all_data[impl_modeler][impl_curator] = pval
+            except KeyError:
+                post_all_data[impl_modeler] = {impl_curator: pval}
+        for impl_name in post_all_data:
+            efect_fp = os.path.join(_experiment_dir, basic.output_subdir_efect, impl_name, basic.efect_report_name)
+            with open(efect_fp, 'r') as f:
+                post_all_data[impl_name][impl_name] = json.load(f)["errorMetricMean"]
+        _post_all(post_all_data,
+                  os.path.join(_experiment_dir, basic.output_subdir_post, basic.output_subdir_compare),
+                  _output_fexts,
+                  fig_dpi)
 
 
 def do_compare(_experiment_dir: str,
@@ -230,33 +299,17 @@ def do_compare(_experiment_dir: str,
     for modeler_impl, curator_impl in jobs:
         logger.info(f'Working: {modeler_impl}, {curator_impl}')
 
-        modeler_rep_fp = os.path.join(target_dir, modeler_impl, target_prefix + basic.efect_report_name)
+        modeler_res = load_results(basic.get_results_raw(_experiment_dir)[modeler_impl])
+
         curator_rep_fp = os.path.join(target_dir, curator_impl, target_prefix + basic.efect_report_name)
         curator_smp_fp = os.path.join(target_dir, curator_impl, target_prefix + basic.efect_sampling_name)
 
-        with open(modeler_rep_fp, 'r') as f:
-            modeler_rep = libssr.EFECTReport.from_json(json.load(f))
         with open(curator_rep_fp, 'r') as f:
             curator_rep = libssr.EFECTReport.from_json(json.load(f))
 
-        if modeler_rep.sample_size != curator_rep.sample_size:
-            logger.error(f'Sample sizes are not equal: {modeler_rep.sample_size, curator_rep.sample_size}')
-            continue
-        elif modeler_rep.simulation_times.shape != curator_rep.simulation_times.shape:
-            msg = 'Number of simulation times are not equal: '
-            msg += f'{modeler_rep.simulation_times.shape, curator_rep.simulation_times.shape}'
-            logger.error(msg)
-            continue
-        elif np.any(modeler_rep.simulation_times != curator_rep.simulation_times):
-            logger.error('Simulation times are not equal')
-            continue
-        elif modeler_rep.variable_names != curator_rep.variable_names:
-            logger.error(f'Variable names are not equal: {modeler_rep.variable_names}, {curator_rep.variable_names}')
-            continue
-
         curator_smp = pd.read_csv(curator_smp_fp, skiprows=1, header=None).iloc[:, 1].to_list()
 
-        res = _compare_reports(modeler_rep, curator_rep, curator_smp)
+        res = _compare_results(modeler_res, curator_rep, curator_smp)
         res[basic.comparison_key_modeler] = modeler_impl
         res[basic.comparison_key_curator] = curator_impl
         output_data.append(res)
